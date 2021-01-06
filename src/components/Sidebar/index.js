8d402fa53331cc3274dd8ea78163011606b9ff9e
vietoba <buixuanviet.dev@gmail.com>
2021-01-06 12:21:17 +0700

    setup project

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <SidebarContainer>
+            <Icon>
+                <CloseIcon />
+            </Icon>
+        </SidebarContainer>
+    )
+}
+
+export default Sidebar
